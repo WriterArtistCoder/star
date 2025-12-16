125194e9f404367dad6c2939a99b435ecd618ac4
Percy Ghenburg <41657434+WriterArtistCoder@users.noreply.github.com>
2025-12-16 13:17:29 -0800

    Created boot script

---
+++ boot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "#/> Set root password: "
+read -sp pass
+echo "\n#/> Confirm password: "
+read -sp pass2
+echo -e "\n#/> Set username: "
+read user
+echo -e "\n:/> Let's get things set up for you."
